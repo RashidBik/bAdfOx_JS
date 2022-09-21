7b84bbc3d8004bebeb9f016b53405c5fe1fd0e0a
rashidbik <abdurashid.bigzad1999@gmail.com>
2022-09-21 22:11:06 +0430

    home page main data was init

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,8 +3,19 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home is here
-      <p>go</p>
+        <a href='/'>Logo</a>
+        <a href='/play'>Play</a>
+        <a href='/learn'>Learn</a>
+        <a href='/about'>About</a>
+        <section>
+        What is the Citizen Developer Practitioner eLearning course?
+Citizen Developer Practitioner is for the “doers.” It provides the tools and methodologies needed to
+efficiently create effective and scalable applications using low-code and no-code platforms, solving
+the problems that organizations face. In this self-paced online course, you will learn how to determine
+if your problem can be solved using citizen development, effectively brainstorm solutions, evaluate
+the challenges that
+        </section>
+
     </div>
   )
 }
