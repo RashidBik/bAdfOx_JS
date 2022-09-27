04db75dc080f514a17f0937b7df5f1d0a05ab10d
rashidbik <abdurashid.bigzad1999@gmail.com>
2022-09-28 01:11:29 +0430

    router changed again

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
 import About from './Pages/About'
 import Home from './Pages/Home'
 import Learn from './Pages/Learn'
@@ -10,27 +8,15 @@ import Play from './Pages/Play'
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/play",
-      element: <Play />,
-    },
-    {
-      path: "/learn",
-      element: <Learn />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-  ]);
+
   return (
     <div className="">
-      <RouterProvider router={router} />
+      <Routes>
+        <Route index="/" exact element={<Home />} />
+        <Route path="/play" exact element={<Play />} />
+        <Route path="/learn" exact element={<Learn />} />
+        <Route path="/about" exact element={<About />} />
+      </Routes>
     </div>
   )
 }
